81758f9be59d36590d1ca3497b1d0143cf3b8f95
mizan21 <mizanorrahman398@gmail.com>
2024-08-18 18:26:12 +0600

    Navbar part polishing

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,75 +1,188 @@
+import { GoChevronDown } from "react-icons/go";
+import { MdOutlineFileDownload } from "react-icons/md";
+import logo from "../../../assets/logo/logo.png"; // Adjust the path based on your project structure
+
 const Navbar = () => {
-  return (
-    <div className="">
-      <div className="navbar ">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
+  const onButtonClick = () => {
+    fetch("/assets/documents/FCL_Brochure.pdf").then((response) => {
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        const alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "FCL_Brochure.pdf";
+        alink.click();
+      });
+    });
+  };
+
+  const navItems = (
+    <>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/about">Explore</a>
+      </li>
+      <li className="relative group">
+        <span className="cursor-pointer flex items-center">
+          Solution <GoChevronDown className="ml-1 font-bold text-2xl" />
+        </span>
+        <ul className="absolute lg:left-[-600px] lg:w-[1530px] mt-10 hidden bg-white text-black shadow-md rounded-md group-hover:block p-2">
+          <div className="flex justify-center items-center gap-6 py-10">
+            <a href="/sub-company">
+              <div className="card bg-base-100 w-96 shadow-xl overflow-hidden">
+                <figure>
+                  <img
+                    className="w-52 h-52"
+                    src="https://i.postimg.cc/Z57dqvzV/Logo-Man-Enterpirse.jpg"
+                    alt="M/S MAN ENTERPRISE"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="text-sm text-center">M/S MAN ENTERPRISE</h2>
+                </div>
+              </div>
+            </a>
+
+            <div className="card bg-base-100 w-96 shadow-xl overflow-hidden">
+              <figure>
+                <img
+                  className="w-52 h-52 "
+                  src="https://i.postimg.cc/nLftB0xz/Screenshot-from-2024-08-12-12-16-43.png"
+                  alt="Shoes"
                 />
-              </svg>
+              </figure>
+
+              <div className="card-body">
+                <h2 className="text-sm text-center">
+                  MR DRILLING & CONSTRUCTION LTD.
+                </h2>
+              </div>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+
+            <div className="card bg-base-100 w-96 shadow-xl overflow-hidden">
+              <figure>
+                <img
+                  className="w-52 h-52 "
+                  src="https://i.postimg.cc/DZq018RV/Screenshot-from-2024-08-12-12-09-21.png"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="text-sm text-center">M/S NACALA ENTERPRISE</h2>
+              </div>
+            </div>
+          </div>
+        </ul>
+      </li>
+      <li className="relative group">
+        <span className="cursor-pointer flex items-center">
+          Services <GoChevronDown className="ml-1 font-bold text-2xl" />
+        </span>
+
+        <ul className="absolute ease-in duration-1000 lg:left-[-800px] lg:w-[1530px] mt-10 hidden bg-white text-black shadow-md rounded-md group-hover:block p-2">
+          <div className="flex justify-center items-center gap-6 py-10">
+            <div className="card bg-base-100 w-96 shadow-xl">
+              <a href="/list-equipments">
+                <figure>
+                  <img
+                    className="w-full h-52 rounded-xl"
+                    src="https://t4.ftcdn.net/jpg/02/49/04/93/360_F_249049329_BpYUPt5TGST3qTTfOJVpTakAdCLFzcg1.jpg"
+                    alt="Shoes"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Equipment list</h2>
+                </div>
+              </a>
+            </div>
+
+            <div className="card bg-base-100 w-96 shadow-xl">
+              <a href="/photo-equipments">
+                <figure>
+                  <img
+                    className="w-full h-52 rounded-xl"
+                    src="https://nmccat.com/wp-content/uploads/2020/09/Cat-Backhoe-1.jpg"
+                    alt="Shoes"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Equipment Photo&apos;s</h2>
+                </div>
+              </a>
+            </div>
+          </div>
+        </ul>
+      </li>
+
+      <li>
+        <a href="/projects">Blogs</a>
+      </li>
+      <li>
+        <a href="/career">Career</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="bg-gradient-to-r from-[#000F1A] via-[#0080E5] to-[#000F1A] fancyNav sticky top-0 z-50 text-white shadow-md">
+      <div className="navbar flex justify-between items-center">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="navbar-start flex items-center">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                {navItems}
+              </ul>
+            </div>
+            <a href="/" className="text-5xl ">
+              <img
+                src={logo}
+                className="w-20"
+                title="Farees Construction Limited (FCL)"
+                alt="FCL logo"
+              />
+            </a>
+          </div>
+          <div className="navbar-center hidden lg:flex flex-grow">
+            <ul className="menu menu-horizontal px-1 flex items-center text-[18px]">
+              {navItems}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+          <div className="navbar-end flex items-center">
+            <a
+              onClick={onButtonClick}
+              href="#"
+              className="bg-black text-white hover:bg-green-700 hover:text-white duration-500 rounded-full px-5 py-2 flex items-center cursor-pointer"
+            >
+              Brochure <MdOutlineFileDownload className="ml-2 text-xl" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
