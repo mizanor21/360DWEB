03bf2238a563367c16d57711e0df1e683c5782cb
mizan21 <mizanorrahman398@gmail.com>
2024-08-19 12:57:24 +0600

    Banner part added

--- src/components/Home/Root.jsx
+++ src/components/Home/Root.jsx
@@ -1,10 +1,12 @@
 import About from "./About/About";
 import Explore from "./Explore/Explore";
 import Goal from "./Goal/Goal";
+import Hero from "./Hero/Hero";
 
 const Root = () => {
   return (
     <div>
+      <Hero />
       <About />
       <Goal />
       <Explore />
